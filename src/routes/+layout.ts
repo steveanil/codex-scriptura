8e85c1e98c9c8decf0f14ad79ae8b913e7421196
Steve Anil John <steveanil2003@gmail.com>
2026-03-24 20:25:42 -0700

    feat(ui): add PWA service worker and static adapter

---
+++ src/routes/+layout.ts
@@ -0,0 +1,4 @@
+// This forces SvelteKit into Single Page Application (SPA) / Static Site Generation (SSG) mode.
+// It generates a single static HTML shell that is perfectly suited for offline caching.
+export const prerender = true;
+export const ssr = false;
